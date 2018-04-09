679a2205703fcf3721b705602da4ebae1caa686c
John <byon34@gmail.com>
2018-04-08 17:22:17 -0700

    my maze

---
+++ Assets/UdacityVR/Scripts/Door.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour 
+{
+    // Create a boolean value called "locked" that can be checked in OnDoorClicked()
+    public bool locked;
+    // Create a boolean value called "opening" that can be checked in Update() 
+    public bool opening;
+    //public bool yesKey;
+    public AudioSource doorSource;
+    public AudioClip [] soundFiles;
+
+    void Start()
+    {
+        locked = true;
+        opening = false;
+        Debug.Log("start" + locked);
+    } 
+    void Update() {
+            // If the door is opening and it is not fully raised
+            // Animate the door raising up
+            if (opening == true)
+            {
+                if (transform.position.y <= 8.4f)
+                { transform.Translate(0, 2.5f * Time.deltaTime, 0, Space.World); }
+            } 
+    }
+
+    public void Unlock()
+    {
+        // You'll need to set "locked" to false here
+        //if(yesKey == true)
+        locked = false;
+    }
+
+    public void OnDoorClicked()
+    {
+        // If the door is clicked and unlocked
+        // Set the "opening" boolean to true
+        {
+            if (locked == false)
+            {
+                opening = true;
+                doorSource.clip = soundFiles[1];
+                doorSource.Play();
+            }
+            // (optionally) Else
+            // Play a sound to indicate the door is locked
+            else
+            {
+                doorSource.clip = soundFiles[0];
+                doorSource.Play();
+            }
+        }
+    }
+}
---
+++ Assets/UdacityVR/Scripts/Key.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour 
+{
+    //Create a reference to the KeyPoofPrefab and Door
+    public GameObject keyPoof;
+    public GameObject door;
+    //static bool keyCollected;
+
+	void Update()
+	{
+		//Not required, but for fun why not try adding a Key Floating Animation here :)
+	}
+
+	public void OnKeyClicked()
+	{
+        // Instatiate the KeyPoof Prefab where this key is located
+        // Make sure the poof animates vertically
+        Object.Instantiate(keyPoof, transform.position, Quaternion.Euler(-990f, 0, 0));
+        // Call the Unlock() method on the Door
+        // Set the Key Collected Variable to true
+        //keyCollected = true;
+        door.GetComponent<Door>().Unlock();
+        //door.Unlock();
+        // Destroy the key. Check the Unity documentation on how to use Destroy
+        Destroy(gameObject);    
+
+    }
+
+}
